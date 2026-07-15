8df736318206f79fb1b6efafa541c97fee24479c
Rinko Vadra <rinkobhadra08@gmail.com>
2026-07-16 05:20:08 +0600

    Updated student information

--- main.cpp
+++ main.cpp
@@ -4,9 +4,9 @@ using namespace std;
 int main()
 {
     cout << "Student Information" << endl;
-    cout << "Name: Your Name" << endl;
-    cout << "Student ID: Your ID" << endl;
-    cout << "Section: Your Section" << endl;
-
+    cout << "Name: Rinko Rani Vadra" << endl;
+    cout << "Student ID: 24-58043-2" << endl;
+    cout << "Section: DD" << endl;
+    cout<<"Favorite Language: C++"<<endl;
     return 0;
 }
