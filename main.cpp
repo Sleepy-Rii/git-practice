02557ccc4809665465cab5661b0559c1a7aa9e97
Sleepy-Rii <sleepyrii08@gmail.com>
2026-07-16 04:53:57 +0600

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Student Information" << endl;
+    cout << "Name: Your Name" << endl;
+    cout << "Student ID: Your ID" << endl;
+    cout << "Section: Your Section" << endl;
+
+    return 0;
+}
